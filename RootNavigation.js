b3cc565fadb91c7a919a63539680bfaef8b4c613
Talha Javed <39768820+Talha-Jawed@users.noreply.github.com>
2022-01-10 19:16:56 +0500

    updated navigationRef condition
    sometimes navigationRef is getting null that's why I am implement is condition

--- RootNavigation.js
+++ RootNavigation.js
@@ -3,5 +3,7 @@ import * as React from 'react';
 export const navigationRef = React.createRef();
 
 export function navigate(name, params) {
-  navigationRef.current?.navigate(name, params);
+  if(navigationRef && navigationRef.current && navigationRef.current.navigate){
+        navigationRef.current?.navigate(name, params);
+    }
 }
